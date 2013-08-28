0b473bf2de55c7c203911f19fb68ac19cc6761f8
Timothy Cochran <timothy.cochran@gmail.com>
2013-08-28 12:23:34 +0300

    removing homebrew receipe [#55630434]

--- solo-bootstrap.sh
+++ solo-bootstrap.sh
@@ -4,3 +4,6 @@ curl -L https://get.rvm.io | bash
 source /etc/profile.d/rvm.sh
 rvm install 1.9.3
 gem install chef ruby-shadow --no-ri --no-rdoc
+sudo apt-get install git
+cd ~/
+git clone https://github.com/unicefuganda/devtrac2-provisioning.git
