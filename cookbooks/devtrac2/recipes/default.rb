9b7f00a71b0ba481d778b44d3181da10014ee3a8
Timothy Cochran <timothy.cochran@gmail.com>
2013-08-28 12:59:39 +0300

    add git checkout and apache config to receipe

--- cookbooks/devtrac2/recipes/default.rb
+++ cookbooks/devtrac2/recipes/default.rb
@@ -6,10 +6,39 @@
 #
 # All rights reserved - Do Not Redistribute
 
-packages = %w{python-pip git apache2.2 libapache2-mod-wsgi}
+SERVER_NAME = 127.0.0.1
+
+packages = %w{python-pip apache2.2 libapache2-mod-wsgi}
 
 packages.each do |package_name|
 	package package_name do
 		action :install 
 	end
 end
+
+git "/var/www/devtrac2" do
+	repository "https://github.com/unicefuganda/devtrac2.git"
+	action :checkout
+end
+
+conf_content = <<-eos
+<VirtualHost *>
+    ServerName <SERVER_NAME>
+
+    WSGIDaemonProcess devtrac2 threads=5
+    WSGIScriptAlias / /var/www/devtrac2/.wsgi
+
+    <Directory /var/www/devtrac2>
+        WSGIProcessGroup devtrac2
+        WSGIApplicationGroup %{GLOBAL}
+        Order deny,allow
+        Allow from all
+    </Directory>
+</VirtualHost>
+<< eos
+
+file "/etc/apache2/httpd.conf" do 
+	content conf_content.gsub(/<SERVER_NAME>/, SERVER_NAME)
+	action :create_if_missing
+end
+
